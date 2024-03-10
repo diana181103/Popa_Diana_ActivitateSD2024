54aa468ba32cd87269d6c918809084a03f04f214
Diana <dianapopa1811@gmail.com>
2024-03-10 15:04:14 +0200

    task3<revizuit>

--- Popa_Diana_ActivitateSD2024/task3.c
+++ Popa_Diana_ActivitateSD2024/task3.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<malloc.h>
 #include<string.h>
@@ -33,6 +33,29 @@ void afisareVectorDepozit(struct Depozit* vectorDepozite, int nr) {
 	}
 }
 
+void salvareInFisier(struct Depozit depozit, const char* numeFisier) {
+	FILE* file = fopen(numeFisier, "w");
+	fprintf(file, "%d,%s,%d", depozit.cod, depozit.nume, depozit.nrProduse);
+	for (int i = 0; i < depozit.nrProduse; i++) {
+		fprintf(file, ",%f", depozit.preturi[i]);
+	}
+	fprintf(file, "\n");
+	fclose(file);
+}
+
+void salvareVectorInFisier(struct Depozit* depozit, int nrDepozite, const char* numeFisier) {
+	FILE* file = fopen(numeFisier, "w");
+	for (int j = 0; j < nrDepozite; j++) {
+		fprintf(file, "%d,%s,%d", depozit[j].cod, depozit[j].nume, depozit[j].nrProduse);
+		for (int i = 0; i < depozit[j].nrProduse; i++) {
+			fprintf(file, ",%f", depozit[j].preturi[i]);
+		}
+		fprintf(file, "\n");
+	}
+
+	fclose(file);
+}
+
 
 struct Depozit* adaugareInVector(struct Depozit* depozit, struct Depozit d, int* nrDep) {
 	struct Depozit* aux = (struct Depozit*)malloc(sizeof(struct Depozit) * ((*nrDep) + 1));
@@ -53,6 +76,12 @@ struct Depozit* citireFisier(const char* numeFisier, int* nrDep) {
 	char delimitare[] = ",\n";
 	struct Depozit* depozit = NULL;
 	(*nrDep) = 0;
+	if (file == NULL) {
+		printf("Eroare: Nu se poate deschide fisierul %s\n", numeFisier);
+		perror("fopen");
+		return NULL;
+	}
+
 	while (fgets(buffer, 100, file) != NULL) {
 		char* element = strtok(buffer, delimitare);
 		struct Depozit d;
@@ -62,10 +91,10 @@ struct Depozit* citireFisier(const char* numeFisier, int* nrDep) {
 		strcpy(d.nume, element);
 		element = strtok(NULL, delimitare);
 		d.nrProduse = atoi(element);
-		d.preturi = malloc(sizeof(float) * d.nrProduse+1);
+		d.preturi = malloc(sizeof(float) * d.nrProduse + 1);
 		for (int i = 0; i < d.nrProduse; i++) {
 			element = strtok(NULL, delimitare);
-			d.preturi[i] = atoi(element);
+			d.preturi[i] = atof(element);
 		}
 		depozit = adaugareInVector(depozit, d, nrDep);
 
@@ -78,10 +107,37 @@ struct Depozit* citireFisier(const char* numeFisier, int* nrDep) {
 
 int main() {
 
-	struct Depozit* depozit= NULL;
+	struct Depozit* depozit = NULL;
 	int nrDep = 0;
-	depozit = citireFisier("depozit.txt", &nrDep);
+	depozit = citireFisier("depozite.txt", &nrDep);
 	afisareVectorDepozit(depozit, nrDep);
-	
+
+	struct Depozit depozit1;
+	depozit1.cod = 1;
+	depozit1.nume = "Depozit1";
+	depozit1.nrProduse = 3;
+	depozit1.preturi = (float*)malloc(3 * sizeof(float));
+	depozit1.preturi[0] = 10.5;
+	depozit1.preturi[1] = 20.3;
+	depozit1.preturi[2] = 15.8;
+
+	salvareInFisier(depozit1, "depozit.txt");
+
+	struct Depozit depozit2;
+	depozit2.cod = 2;
+	depozit2.nume = "Depozit2";
+	depozit2.nrProduse = 3;
+	depozit2.preturi = (float*)malloc(3 * sizeof(float));
+	depozit2.preturi[0] = 9.5;
+	depozit2.preturi[1] = 10.3;
+	depozit2.preturi[2] = 1.8;
+
+	struct Depozit* vDepozit = (struct Depozit*)malloc(sizeof(struct Depozit) * 2);
+	vDepozit[0] = depozit1;
+
+	vDepozit[1] = depozit2;
+
+	salvareVectorInFisier(vDepozit, 2, "vectorDepozite.txt");
+
 
 }
